257521bacb8042e2c408ddc8b7f3cb50fa2b6555
Aalek <appinfore@gmail.com>
2014-03-10 16:01:47 +0000

    Create Appdelegate.h

---
+++ Appdelegate.h
@@ -0,0 +1,15 @@
+/
+//  AppDelegate.h
+//  Beacon Demo
+//
+//  Created by Aalek Dixit on 10/03/2014.
+//  Copyright (c) 2014 Aalek Dixit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
